860d70f58c21ab900831f80962f7a1717b5d99f0
jasmynejean-remy <jasmynej14@gmail.com>
2025-03-14 22:08:06 -0400

    temp home page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+router.get('/', (req, res) => {
+  res.send("Ariventures Data")
+})
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/home', async function(req, res, next) {
   const countriesDb = await axios.get("http://localhost:4000/countries");
   res.render('index', { countries: countriesDb.data });
 });

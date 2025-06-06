6c3c75de4708d7818d6bd3a7da60912cfe9e079a
jasmynejean-remy <jasmynej14@gmail.com>
2025-06-05 21:19:40 -0400

    upload cover image

--- routes/images.js
+++ routes/images.js
@@ -68,4 +68,32 @@ router.post('/profile',upload.single('profileImage'), async (req, res) => {
     }
 })
 
+router.post('/cover', upload.single('coverImage'), async (req, res) => {
+    try {
+        const profile_id = req.query.id;
+        if (!req.file) {
+            return res.status(400).json({ error: 'No image file provided' });
+        }
+
+        const { buffer, mimetype, originalname } = req.file;
+
+        // Upload the image buffer to Vercel Blob
+        const blob = await put(`/cover_images/${originalname}`,buffer, {
+            access: 'public',
+        })
+
+        const {data, error} = await supabase
+            .from("user_profile")
+            .update({cover_image:blob.url})
+            .eq("id", profile_id)
+            .select("*")
+        if (error) throw error;
+        res.json(data);
+    }
+    catch (error) {
+        console.error('Upload error:', error);
+        res.status(500).json({ error: 'Error uploading image' });
+    }
+})
+
 module.exports = router;

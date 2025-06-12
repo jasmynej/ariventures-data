9f0a14adb64544f5f4227893d5bdc53ee134b44f
jasmynejean-remy <jasmynej14@gmail.com>
2025-06-12 19:48:47 -0400

    country image upload

--- routes/images.js
+++ routes/images.js
@@ -96,4 +96,32 @@ router.post('/cover', upload.single('coverImage'), async (req, res) => {
     }
 })
 
+
+router.post('/country',upload.single('countryImage'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: 'No image file provided' });
+        }
+
+        const { buffer, mimetype, originalname } = req.file;
+
+        // Upload the image buffer to Vercel Blob
+        const blob = await put(`/cities/${originalname}`,buffer, {
+            access: 'public',
+        })
+
+        const {data, error} = await supabase
+            .from('countries')
+            .update({img:blob.url})
+            .eq("id", req.query.id)
+            .select('*')
+
+        if (error) throw error;
+
+        res.json(data);
+    } catch (error) {
+        console.error('Upload error:', error);
+        res.status(500).json({ error: 'Error uploading image' });
+    }
+})
 module.exports = router;
